9e7a7395617673c2cbc16f2f6c054943fc5657d6
Jordon <jcall@cybersugarstudios.com>
2025-11-05 18:59:45 -0800

    Started work on the Gearbox Puzzle, implemented camera tracking and zoom on the player character placeholder.

---
+++ Scripts/gearbox_puzzle.gd
@@ -0,0 +1,14 @@
+extends Control
+
+#region Variable Declaration
+# SCENE NODES
+@onready var startingGear
+@onready var endingGear
+@onready var gearOne
+@onready var gearTwo
+@onready var gearThree
+
+# PRELOADS
+
+
+#endregion
