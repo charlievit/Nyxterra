78510cf8d2d4dde74d99863971d2130e83f190d9
Kin Yin Ho <hobenson57@gmail.com>
2025-11-20 06:20:34 -0800

    Manually put save&load system to the game

---
+++ Scripts/save_load/save_data.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name SaveData
+
+@export var player_position: Vector2 = Vector2.ZERO
+@export var current_scene_path: String = ""
+
+# Add more as your game grows:
+@export var morse_is_solved: bool = false
+@export var player_floor: int = 3
