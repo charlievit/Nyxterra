a2dc800e10016f2fe21ca5f913464114368e086a
Charlie Vaughn <charlixxx2002@gmail.com>
2025-12-01 16:57:39 -0800

    bugfix/CRITICAL_BROWSER_BUGFIX2

--- Scripts/LockboxPuzzle/lockbox_minigame.gd
+++ Scripts/LockboxPuzzle/lockbox_minigame.gd
@@ -123,7 +123,9 @@ func _on_read_letter_button_pressed() -> void:
 	sfxPlayer.play()
 	GameManager.CompleteTask(currentTaskID)
 	readLetterButton.disabled = true
-	Dialogic.start("Day_5 Final Letter")
+	var dialogic_node = Dialogic.start("Day_5 Final Letter")
+	
+	await dialogic_node.timeline_ended
 	
 	SceneLoader.change_scene_with_loading(goodEndingScenePath)
 
